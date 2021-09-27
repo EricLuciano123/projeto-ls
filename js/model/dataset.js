172fb6b8816f8f1ad4d9dba2e7fa0614f5eaaccc
EricLuciano123 <39489684+EricLuciano123@users.noreply.github.com>
2021-09-27 11:45:32 -0300

    Add files via upload

---
+++ js/model/dataset.js
@@ -0,0 +1,22 @@
+const dataset = [
+    {
+        id: '1',
+        image: '../../img/calculadora.PNG',
+        textodesc: 'Calculadora desenvolvida em HTML5, CSS3 e JavaScript',
+        endereco: '../../pages/calculadora.html',
+    },
+    {
+        id: '2',
+        image: '../../img/site-pessoas-depressao.PNG',
+        textodesc: 'Site desenvolvido em HTML5 e CSS3 para ajudar pessoas com Depressão e Ansiedade',
+        endereco: 'https://projeto-1.fabianabeda.repl.co',
+    },
+    {
+        id: '3',
+        image: '../../img/elementos-dinamicos.PNG',
+        textodesc: 'Elementos Dinâmicos: Desenvolvido em HTML5, CSS3 e JavaScript. Design responsivo com elementos dinamicos.',
+        endereco: 'https://elementos-dinamicos.ericluciano.repl.co/',
+    },
+];
+
+export default dataset;
---
+++ js/script.js
@@ -0,0 +1,13 @@
+function alternaprojeto() {
+  let projetos = document.querySelector('.projetos');
+  projetos.setAttribute('style', 'Display: none;');
+  let jogos = document.querySelector('.jogos');
+  jogos.setAttribute('style','Display: block')
+};
+
+function alternarjogo() {
+  let projetos = document.querySelector('.projetos');
+  projetos.setAttribute('style', 'Display: block;');
+  let jogos = document.querySelector('.jogos');
+  jogos.setAttribute('style','Display: none')
+};

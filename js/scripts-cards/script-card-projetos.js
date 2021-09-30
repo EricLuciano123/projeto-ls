0c7f0f3fb80e1b666bc729214cb4e03ab2cc70a4
EricLuciano123 <39489684+EricLuciano123@users.noreply.github.com>
2021-09-30 03:13:44 -0300

    Add files via upload

--- js/scripts-cards/script-card-projetos.js
+++ js/scripts-cards/script-card-projetos.js
@@ -1,27 +1,26 @@
 import dataset from "../model/dataset.js";
 
-for ( let cards of dataset ) {
-    const itens = document.querySelector('.itens');
-    let card = criadordecards(cards);
-    itens.insertAdjacentHTML('beforeend', card);
-  };
+const itens = document.querySelector('.itens');
+
+dataset.map(criadordecards);
 
 function criadordecards(cards) {
-return `
+    const card = `
     <div class="item">
-        <div class="cabecalho-item">
-            <span>
-                Projeto ${cards.id}
-            </span>
-        </div>
-        <div class="meio-item">
-            <div class="item-img-projeto"><img src="${cards.image}" alt=""></div>  
-        </div>
-        <div class="bottom-item">
-            <p class="description">Descrição</p>
-            <p class="text-description">${cards.textodesc}</p>
-        </div>
-        <div class="botoes-item">
-            <a class="interacao-item" href="${cards.endereco}">Ver projeto</a>
+    <div class="cabecalho-item">
+    <span>
+    Projeto ${cards.id}
+    </span>
+    </div>
+    <div class="meio-item">
+    <div class="item-img-projeto"><img src="${cards.image}" alt=""></div>  
+    </div>
+    <div class="bottom-item">
+    <p class="description">Descrição</p>
+    <p class="text-description">${cards.textodesc}</p>
+    </div>
+    <div class="botoes-item">
+    <a class="interacao-item" href="${cards.endereco}">Ver projeto</a>
     </div>`
+    itens.insertAdjacentHTML('beforeend', card);
 };
